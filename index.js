6c7a0e19f94c730e66a3b3d5f497e9af3cbe340e
Cee1jay <chantalwanjiru9@gmail.com>
2022-06-06 18:06:39 +0300

    parameters

---
+++ index.js
@@ -0,0 +1,18 @@
+function introduction (name) {
+    const message = `Hi, my name is ${name}.`;
+    return message
+};
+
+function introductionWithLanguage(name, language) {
+    const message = `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return message
+};
+
+function introductionWithLanguageOptional(name, language) {
+    const message = `Hi, my name is ${name} and I am learning to program in ${language}`;
+    return message;
+};
+
+function introductionWithLanguageOptional(name, language = "JavaScript") {
+    const message = `Hi, my name is ${name} and I am learning to program in ${language}.`;
+    return message
